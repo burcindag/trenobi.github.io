27a24fa31c575c43170fa8b0f5a3775760126ac9
bdagistan <66089529+burcindag@users.noreply.github.com>
2024-09-04 17:38:10 +0300

    Update script.js
    lgVideo

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Initialize the lightGallery plugin for each subgallery
+    /*
     lightGallery(document.getElementById('youtubegallery1'), {
         plugins: [lgShare], // Enable the share plugin
         share: true, // Ensure share is enabled
         download: true, // Keep download enabled
     });
+
+    */
+
+    lightGallery(document.getElementById('youtubegallery'), {
+        plugins: [lgVideo],
+        selector: 'a',
+        videojs: true,
+    });
+    
     lightGallery(document.getElementById('lightgallery1'), {
         plugins: [lgShare], // Enable the share plugin
         share: true, // Ensure share is enabled
